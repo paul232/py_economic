744c551b961d63adb66d4908a8bda8aa2d40bcce
Pavel <pasha.gubin232@gmail.com>
2020-04-01 00:49:56 +0300

    scikit-learn init

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-from Intro.processes.manufacture import *
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
-product_creation(100, 100, 50)
+melbourne_file_path = './input/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path)
 
+print(melbourne_data.columns)
+
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+
+y = melbourne_data.Price
+X = melbourne_data[melbourne_features]
+
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X, y)
+
+print(X.describe())
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
